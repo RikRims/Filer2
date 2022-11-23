19bb568b51cffb310cd5cb2f5c421fa52c81dcd0
kiril <kiril@DESKTOP-NJE0QA0>
2022-11-23 15:10:01 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Filer2
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (check_path.IsChecked == false)
+                New_addres.Visibility = Visibility.Visible;
+            else New_addres.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void btn_addres_old_Click(object sender, RoutedEventArgs e)
+        {
+            var folderPicker = new Windows.Storage.Pickers.FolderPicker();
+            folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add("*");
+
+            Windows.Storage.StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                Windows.Storage.AccessCache.StorageApplicationPermissions.
+                FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+                this.Address.Text = folder.Path;
+            }
+            else
+            {
+                this.Address.Text = "Operation cancelled.";
+            }
+        }
+
+        private void btn_addres_old_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
