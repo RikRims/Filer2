02a4b4a0baa10abd50240db15c3686baf4b2ace5
RikRims <kirill.smirik1@gmail.com>
2023-02-17 13:24:37 +0500

    Code Review

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,7 @@ namespace Filer2
 {
     public partial class MainWindow : Window
     {
-        List<string> typeFiles = new List<string>();
+        private List<string> typeFiles = new();
 
         public MainWindow()
         {
@@ -22,25 +22,20 @@ namespace Filer2
             Version version = Assembly.GetExecutingAssembly().GetName().Version;
             textBloclVersion.Text = Convert.ToString(version);
 
-            //по дефолту выбирается рабочий стол как рабочая область
+            //по дефолту выбирается рабочий стол и папка в "Документы" как рабочие области
             addresOld.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        }
-
-        //показать/спрятать выбор конечной папки для перемещения файлов
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            if (check_path.IsChecked == false)
-                New_addres.Visibility = Visibility.Visible;
-            else New_addres.Visibility = Visibility.Collapsed;
+            addresNew.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Filer2";
         }
 
         //выбор папки с файлами
         private void Btn_addres_old_Click(object sender, RoutedEventArgs e)
         {
-            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
-            dialog.InitialDirectory = "C:";
-            dialog.IsFolderPicker = true;
-            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            CommonOpenFileDialog dialog = new()
+            {
+                InitialDirectory = "C:",
+                IsFolderPicker = true
+            };
+            if(dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
                 addresOld.Text = dialog.FileName;
             }
@@ -49,13 +44,14 @@ namespace Filer2
         //выбор конечной папки для перемещения файлов
         private void Btn_addres_new_Click(object sender, RoutedEventArgs e)
         {
-            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
-            dialog.InitialDirectory = "C:";
-            dialog.IsFolderPicker = true;
-            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            CommonOpenFileDialog dialog = new()
+            {
+                InitialDirectory = "C:",
+                IsFolderPicker = true
+            };
+            if(dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
                 addresNew.Text = dialog.FileName;
-                check_path.IsEnabled = false;
             }
         }
 
@@ -63,28 +59,30 @@ namespace Filer2
         private void AddFiles(object sender, RoutedEventArgs e)
         {
             var chbox = sender as CheckBox;
-            if (((bool)chbox.IsChecked) && (!typeFiles.Contains(chbox.Content)))
+            if(((bool)chbox.IsChecked) && (!typeFiles.Contains(chbox.Content)))
             {
                 typeFiles.Add((string)chbox.Content);
             }
-            else typeFiles.Remove((string)chbox.Content);
+            else
+                typeFiles.Remove((string)chbox.Content);
         }
 
         private void DeleteFile(object sender, RoutedEventArgs e)
         {
             DialogResult dialogResult = System.Windows.Forms.MessageBox.Show("Это действие удаляет файлы полностью (не в корзину!). Продолжить?", "Удаление!", MessageBoxButtons.OKCancel);
-            if (dialogResult == System.Windows.Forms.DialogResult.OK)
+            if(dialogResult == System.Windows.Forms.DialogResult.OK)
             {
-                if (typeFiles.Count == 0)
+                if(typeFiles.Count == 0)
                     System.Windows.Forms.MessageBox.Show("Вы не выбрали форматы файлов!", "Ошибка 1");
-                else foreach (var format in typeFiles)
-                {
-                    string[] _files = Directory.GetFiles(addresOld.Text, format);
-                    foreach (string _file in _files)
+                else
+                    foreach(var format in typeFiles)
                     {
-                        File.Delete(_file);
+                        string[] _files = Directory.GetFiles(addresOld.Text, format);
+                        foreach(string _file in _files)
+                        {
+                            File.Delete(_file);
+                        }
                     }
-                }
             }
         }
 
@@ -94,39 +92,35 @@ namespace Filer2
             string pathDir;
             string nameDir = DateTime.Today.ToString();
             int found = nameDir.IndexOf(" ");
-            string pathFile = System.IO.Path.GetFullPath(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Filer2" + "/log/" + $"{nameDir.Substring(0, found)}.txt");
-            using (StreamWriter log = File.Exists(pathFile) ? File.AppendText(pathFile) : File.CreateText(pathFile))
+            string pathFile = Path.GetFullPath($"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}/Filer2/log/{nameDir[..found]}.txt");
+            using(StreamWriter log = File.Exists(pathFile) ? File.AppendText(pathFile) : File.CreateText(pathFile))
             {
-                string text = ($"{nameDir} --- Операция Перемещения --- {typeFiles.Count} --- {check_path.IsChecked} --- {addresNew.Text}");
+                string text = ($"{nameDir} --- Операция Перемещения --- {typeFiles.Count} --- {addresNew.Text}");
                 log.WriteLine(text);
             }
 
-            if (check_path.IsChecked == true)
-            {
-                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Filer2" + "/" + nameDir.Substring(0, found));
-                pathDir = System.IO.Path.GetFullPath(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Filer2" + "/" + nameDir.Substring(0, found));
-            }
-            else //ошибка 2
-                if (addresNew.Text == "")
+            //ошибка 2
+            if(addresNew.Text == "")
             {
                 System.Windows.Forms.MessageBox.Show("Вы не выбрали путь до хранилища!", "Ошибка 2");
                 return;
             }
-            else pathDir = addresNew.Text;
+            else
+                pathDir = addresNew.Text;
             //ошибка 1
-            if (typeFiles.Count == 0)
-            { 
+            if(typeFiles.Count == 0)
+            {
                 System.Windows.Forms.MessageBox.Show("Вы не выбрали форматы файлов!", "Ошибка 1");
                 return;
             }
             else
             {
-                foreach (var format in typeFiles)
+                foreach(var format in typeFiles)
                 {
                     string[] _files = Directory.GetFiles(addresOld.Text, format);
-                    foreach (string _file in _files)
+                    foreach(string _file in _files)
                     {
-                        string pathFale = pathDir + "\\" + _file.Substring(_file.LastIndexOf("\\") + 1);
+                        string pathFale = pathDir + "\\" + _file[(_file.LastIndexOf("\\") + 1)..];
                         File.Move(@_file, pathFale);
                     }
                 }
@@ -135,23 +129,25 @@ namespace Filer2
         }
 
         //сканировать рабочую область и создать чекбоксы соответствующие файлам
-        private void ScanWorkPath(object sender, RoutedEventArgs e)                                           
+        private void ScanWorkPath(object sender, RoutedEventArgs e)
         {
             CheckBoxConteiner.Children.Clear();
-            List<string> nameCheckBox = new List<string>();
+            List<string> nameCheckBox = new();
 
             string[] _files = Directory.GetFiles(addresOld.Text);
-            foreach (string _file in _files)
+            foreach(string _file in _files)
             {
                 int begin = _file.LastIndexOf(".");
                 nameCheckBox.Add(_file[begin..]);
             }
 
             IEnumerable<string> distinctNameCheckBox = nameCheckBox.Distinct();
-            foreach (string _file in distinctNameCheckBox)
+            foreach(string _file in distinctNameCheckBox)
             {
-                System.Windows.Controls.CheckBox checkBox = new();
-                checkBox.Content = _file;
+                CheckBox checkBox = new()
+                {
+                    Content = _file
+                };
                 checkBox.Click += AddFiles;
                 CheckBoxConteiner.Children.Add(checkBox);
             }
