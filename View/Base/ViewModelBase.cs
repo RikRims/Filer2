eb1c1f71096a6e0df901b004ec4696db06d0ade2
RikRims <kirill.smirik1@gmail.com>
2023-04-02 22:22:02 +0500

    MVVM Pattern

---
+++ View/Base/ViewModelBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filer2.View.Base;
+internal class ViewModelBase
+{
+}
