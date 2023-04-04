5c4493dc22e505a84f1a637dcd417cc0353aa908
RikRims <kirill.smirik1@gmail.com>
2023-04-04 22:33:06 +0500

    Add MainModel

---
+++ Model/MainModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Forms;
+
+namespace Filer2.Model
+{
+    public class MenuItems
+    {
+        public string MenuItem { get; set; }
+        public string MenuImg { get; set; }
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Filer2.View.Base;
+
+namespace Filer2.ViewModel;
+internal class MainWindowViewModel : ViewModelBase
+{
+    
+
+}
